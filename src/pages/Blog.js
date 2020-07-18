17cdb6947043ff9ef9dc3b9d6a27a23f5e883508
Ji Simmons <ji.simmons@yahoo.com>
2020-07-17 19:42:58 -0600

    fixing a screwup

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -16,9 +16,9 @@ export default class Blog extends RC {
     getHistory() {
         axios
        // This is where the data is hosted When local because Conni said so
-            //.get('http://localhost:5555/api/boats')
+            .get('http://localhost:5555/api/boats')
             // when it's live:
-            .get('https://blog-api1.herokuapp.com/') 
+            //.get('https://blog-api1.herokuapp.com/') 
         // Once we get a response and store data, let's change the loading state
         .then(response => {
             console.log(response);
